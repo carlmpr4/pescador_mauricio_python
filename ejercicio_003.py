59bc6edc46209aee7ad7214f567ca10ff10f23de
carlmpr4 <54786861+carlmpr4@users.noreply.github.com>
2023-05-08 16:52:51 -0600

    Add files via upload

---
+++ ejercicio_003.py
@@ -0,0 +1,5 @@
+contrahumano = input("Escribe una contrasenia que sera con la que comparemos: ")
+contrabot =""
+while contrabot != contrahumano:
+    contrabot = input("Introduce la contraseña: ")
+print("Contraseña correcta!")
